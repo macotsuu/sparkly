23c802fcad1d05e796e70abe34554f1b6a065090
Dominik Szamburski <dominikszamburski99@gmail.com>
2023-03-12 20:58:35 +0100

    refactor: using env in config file

--- app/config/database.php
+++ app/config/database.php
@@ -1,9 +1,9 @@
 <?php
 
     return [
-        'hostname' => 'localhost',
-        'port' => 3306,
-        'username' => 'sparkly',
-        'password' => 'sparkly',
-        'database' => 'sparkly'
+        'hostname' => env('DB_HOSTNAME', 'localhost'),
+        'port' => env('DB_PORT', 3306),
+        'username' => env('DB_USERNAME', 'sparkly'),
+        'password' => env('DB_PASSWORD', ''),
+        'database' => env('DB_DATABASE', '')
     ];
