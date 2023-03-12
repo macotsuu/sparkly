854f487596b0a43cbd908ee4a3e7ee131d238445
Dominik Szamburski <dominikszamburski99@gmail.com>
2023-03-12 20:42:47 +0100

    chore: add deps

--- public/web.php
+++ public/web.php
@@ -1,16 +1,7 @@
 <?php
 
-use React\Http\HttpServer;
-use React\Socket\SocketServer;
-use Sparkly\Framework\Foundation\Kernel;
+use Sparkly\Framework\Foundation\Application;
 
 require_once __DIR__ . '/../app/bootstrap.php';
 
-$sparkly = Kernel::getInstance();
-$sparkly->bootstrap();
-
-$http = new HttpServer($sparkly->make('http.kernel'));
-$http->on('error', function (Exception|TypeError $e) {
-    sparkly('logger')->error($e->getMessage());
-    sparkly('logger')->error($e->getTraceAsString());
-})->listen(new SocketServer('0.0.0.0:8080'));
+Application::getInstance()->run();
